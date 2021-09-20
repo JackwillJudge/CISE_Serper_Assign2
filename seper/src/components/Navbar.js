4b31ed0935af893daec504f2e4e3b804848b46f7
unknown <jack.w.judge@gmail.com>
2021-09-20 18:28:00 +1200

    nav bar creation

---
+++ seper/src/components/Navbar.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    const [button, setButton] = useState(true);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }       
+    };
+
+    window.addEventListener('resize', showButton);
+
+    return  (
+    <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className ="nav-logo">
+                  SEPER 
+                </Link>
+                <div className='menu-icon' onClick={handleClick}> 
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                </div>
+                <ul className ={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className = 'nav-iten'>
+                        <Link to='/login' className='nav-links' onClick={closeMobileMenu}>
+                            Search
+                        </Link>
+                    </li>
+                    <li className = 'nav-iten'>
+                        <Link to='/register' className='nav-links' onClick={closeMobileMenu}>
+                            Submit
+                        </Link>
+                    </li>
+                    <li className = 'nav-iten'>
+                        <Link to='/modqueue' className='nav-links' onClick={closeMobileMenu}>
+                            Moderator Queue
+                        </Link>
+                    </li>
+                    <li className = 'nav-iten'>
+                        <Link to='/analqueue' className='nav-links' onClick={closeMobileMenu}>
+                            Analyst Queue
+                        </Link>
+                    </li>
+                </ul>
+                {button && <button buttonStyle='btn--outline'>Login</button>}
+                {button && <button buttonStyle='btn--outline'>Register</button>}
+
+            </div>
+        </nav>
+    </>
+    );
+}
+
+export default Navbar
