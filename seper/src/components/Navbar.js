fd6ec2cfd4f5d389dfa50da6b1ad3fd340fd8636
unknown <jack.w.judge@gmail.com>
2021-09-20 20:22:01 +1200

    nav bar

--- seper/src/components/Navbar.js
+++ seper/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import { Button } from './Button';
 import './Navbar.css';
 
 function Navbar() {
@@ -22,37 +23,36 @@ function Navbar() {
     <>
         <nav className="navbar">
             <div className="navbar-container">
-                <Link to="/" className ="nav-logo">
+                <Link to="/" className ="seper-home">
                   SEPER 
                 </Link>
                 <div className='menu-icon' onClick={handleClick}> 
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
                 </div>
                 <ul className ={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className = 'nav-iten'>
+                    <li className = 'nav-item'>
                         <Link to='/login' className='nav-links' onClick={closeMobileMenu}>
                             Search
                         </Link>
                     </li>
-                    <li className = 'nav-iten'>
+                    <li className = 'nav-item'>
                         <Link to='/register' className='nav-links' onClick={closeMobileMenu}>
                             Submit
                         </Link>
                     </li>
-                    <li className = 'nav-iten'>
+                    <li className = 'nav-item'>
                         <Link to='/modqueue' className='nav-links' onClick={closeMobileMenu}>
                             Moderator Queue
                         </Link>
                     </li>
-                    <li className = 'nav-iten'>
+                    <li className = 'nav-item'>
                         <Link to='/analqueue' className='nav-links' onClick={closeMobileMenu}>
                             Analyst Queue
                         </Link>
                     </li>
                 </ul>
-                {button && <button buttonStyle='btn--outline'>Login</button>}
-                {button && <button buttonStyle='btn--outline'>Register</button>}
-
+                {button && <Button buttonStyle='btn--outline'>Login</Button>}
+                {button && <Button buttonStyle='btn--outline'>Register</Button>}
             </div>
         </nav>
     </>
