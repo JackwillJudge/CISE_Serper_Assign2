b19058282e4f4b92597519e9900ce9d68e343f3f
승열 이 <vulca123@naver.com>
2021-10-08 20:20:34 +1300

    heroku deployment commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // app.js
-
+require("dotenv").config({ path:"./config.env" });
+const path = require('path');
 const express = require("express");
 const connectDB = require("./config/db");
 var cors = require("cors");
@@ -19,12 +20,23 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Hello world!"));
+// app.get("/", (req, res) => res.send("API running"));
 
 // use Routes
 app.use("/api/articles", articles);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/seper/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'seper','build', 'index.html'));
+    })
+} else{
+    app.get("/", (req, res) => res.send("Api running"));
+}
 
 const port = process.env.PORT || 8082;
 
 app.listen(port, () =>
  console.log(`Server running on port ${port}`));
+
