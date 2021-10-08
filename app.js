a0ca18bd8d937ac83c5aec97a0fd93fe77a9e146
승열 이 <vulca123@naver.com>
2021-10-08 20:43:07 +1300

    heroku commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json({ extended: false }));
 // use Routes
 app.use("/api/articles", articles);
 
-if(process.env.NODE_ENV === "development") {
+if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '/seper/build')))
 
     app.get('*', (req, res) => {
